6c184ec88ed2b578e5115dec511fbbbb83309ab6
Arhan <arhanh@nycstudents.net>
2023-04-17 12:35:10 -0400

    s

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    private int x;
+    private int y;
+    private String label;
+
+    public Point ( String label, int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return "Point "+label+" is at x = "+x+", y = "+y;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Point)) {
+            return false;
+        }
+        Point temp = (Point) obj;
+        return temp.x==this.x&&temp.y==this.y;
+    }
 }
